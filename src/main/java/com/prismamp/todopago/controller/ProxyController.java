8feeada566bab2d92c242a82a80dd15e0c1ba71a
Agustín Dib <agustin.dib@intive-fdv.com>
2017-11-16 11:10:25 -0300

    Se agrega funcionalidad para el paralelismo

--- src/main/java/com/prismamp/todopago/controller/ProxyController.java
+++ src/main/java/com/prismamp/todopago/controller/ProxyController.java
@@ -1,8 +1,10 @@
 package com.prismamp.todopago.controller;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,11 +24,28 @@ import com.prismamp.todopago.service.ProxyService;
 public class ProxyController {
 
 	@Autowired
-	ProxyService service;
+	private ProxyService service;
 
 	@RequestMapping(method = RequestMethod.GET, value = "proxy/banco")
-	public ResponseEntity<Map<String, Banco>> findAllBanco() {
+	public ResponseEntity<Map<String, Banco>> findAllBank() {
 
-		return service.findAllBanco();
+		return service.findAllBank();
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "proxy/calculateAsync")
+	public ResponseEntity<Long> calculateAsync() throws InterruptedException {
+
+		return calculateAsyncTime();
+	}
+
+	private ResponseEntity<Long> calculateAsyncTime() throws InterruptedException {
+		long start = System.currentTimeMillis();
+
+		CompletableFuture<ResponseEntity<Map<String, Banco>>> total1 = service.asyncFindAllBank();
+		CompletableFuture<ResponseEntity<Map<String, Banco>>> total2 = service.asyncFindAllBank();
+
+		CompletableFuture.allOf(total1, total2).join();
+
+		return new ResponseEntity<Long>(System.currentTimeMillis() - start, HttpStatus.OK);
 	}
 }
--- src/main/java/com/prismamp/todopago/service/ProxyService.java
+++ src/main/java/com/prismamp/todopago/service/ProxyService.java
@@ -1,10 +1,12 @@
 package com.prismamp.todopago.service;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.prismamp.todopago.configuration.CommandConfiguration;
@@ -21,21 +23,40 @@ import com.prismamp.todopago.restclient.impl.RestClientImpl;
 public class ProxyService {
 
 	@Autowired
-	RestClientImpl restClient;
+	private RestClientImpl restClient;
 
-	public ResponseEntity<Map<String, Banco>> findAllBanco() {
-		String appId = "proxy";
-		String endpointUrl = "/api/BSA/proxy/globales/banco";
+	private String app = "proxy";
+	private String endpoint = "/api/BSA/proxy/globales/banco";
+
+	public ResponseEntity<Map<String, Banco>> findAllBank() {
+		CommandConfiguration conf = new CommandConfiguration();
+		conf.setFallbackEnabled(true);
+		conf.setExecutionTimeoutEnabled(true);
+		conf.setRequestCacheEnabled(false);
+		conf.setExecutionTimeoutInMilliseconds(5000);
+		conf.setCommandName("findAllBankTimeoutFallback");
+
+		ResponseEntity fallback = new ResponseEntity("Fallback response", HttpStatus.INTERNAL_SERVER_ERROR);
+		ResponseEntity<Map<String, Banco>> response = restClient.getForEntity(app, endpoint, Map.class, conf, fallback);
+
+		return response;
+	}
+
+	@Async
+	public CompletableFuture<ResponseEntity<Map<String, Banco>>> asyncFindAllBank() throws InterruptedException {
 
 		CommandConfiguration conf = new CommandConfiguration();
 		conf.setFallbackEnabled(true);
 		conf.setExecutionTimeoutEnabled(true);
 		conf.setRequestCacheEnabled(false);
 		conf.setExecutionTimeoutInMilliseconds(5000);
-		conf.setCommandName("findAllBancoTimeoutFallback");
-		ResponseEntity fallback = new ResponseEntity("This is a fallback response", HttpStatus.INTERNAL_SERVER_ERROR);
-		ResponseEntity response = restClient.getForEntity(appId, endpointUrl, Map.class, conf, fallback);
+		conf.setCommandName("findAllBankTimeoutFallback");
 
-		return response;
+		ResponseEntity fallback = new ResponseEntity("Fallback response", HttpStatus.INTERNAL_SERVER_ERROR);
+		ResponseEntity<Map<String, Banco>> response = restClient.getForEntity(app, endpoint, Map.class, conf, fallback);
+
+		Thread.sleep(5000L);
+		return CompletableFuture.completedFuture(response);
 	}
+
 }
